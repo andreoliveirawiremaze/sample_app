bb3dc04bfa41aedad72d498fbc7a377072307896
Andre Oliveira <andre.oliveira@wiremaze.com>
2015-11-11 15:06:29 +0000

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111102501) do
+ActiveRecord::Schema.define(version: 20151111143451) do
 
 # Could not dump table "users" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
